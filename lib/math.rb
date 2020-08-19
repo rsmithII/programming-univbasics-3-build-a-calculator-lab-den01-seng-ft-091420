4942fb32663182d2d76deec48ee6b268558401cb
Rodney Smith II <r_smith3@hotmail.com>
2020-08-19 21:12:43 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -3,21 +3,21 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  subtraction = num1 - num2
 end
 
 def division(num1, num2)
-
+  division = num1 / num2
 end
 
 def multiplication(num1, num2)
-
+  multiplication = num1 * num2
 end
 
 def modulo(num1, num2)
-
+  modulo = num1 % num2
 end
 
 def square_root(num)
-
+ square_root = num **
 end
